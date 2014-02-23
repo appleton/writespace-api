e809ccc140a54e4fc0bde443e64b5cae27f2985d
Andrew Appleton <andrew@floatleft.com>
2014-02-23 22:47:36 +0000

    New note flow

--- app/pages/notes/index/controller.js
+++ app/pages/notes/index/controller.js
@@ -1,19 +1,27 @@
 'use strict';
 
 angular.module('notes.index', [
+  'ui.router',
   'notes.service'
 ]).controller('NotesIndexController', [
   '$scope',
+  '$state',
   'NotesService',
-  function($scope, NotesService) {
+  function($scope, $state, NotesService) {
     $scope.newNote = {};
-    $scope.name = 'Notes';
     $scope.NotesService = NotesService;
 
+    $scope.toggleNewNoteForm = function() {
+      $scope.showNewNoteForm = true;
+    };
+
     $scope.addNote = function() {
-      NotesService.post($scope.form).then(function() {
+      NotesService.post($scope.form).then(function(resp) {
+        $scope.showNewNoteForm = false;
         delete $scope.form.title;
         delete $scope.form.text;
+
+        $state.go('notes.show', { id: resp.id });
       });
     };
   }
