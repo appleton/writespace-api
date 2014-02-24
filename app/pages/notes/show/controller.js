a2e45d9668e69741a7f0b53256a76981fc8dc907
Andrew Appleton <andrew@floatleft.com>
2014-02-24 15:16:51 +0000

    Respond to synced updates on notes show

--- app/pages/notes/show/controller.js
+++ app/pages/notes/show/controller.js
@@ -4,21 +4,48 @@ angular.module('notes.show', [
   'notes.service'
 ]).controller('NotesShowController', [
   '$scope',
+  '$timeout',
   'note',
   'NotesService',
-  function($scope, note, NotesService) {
+  function($scope, $timeout, note, NotesService) {
+    var id = note._id;
     $scope.note = note;
+
+    function isChanged(revision) {
+      return $scope.note._rev !== _.last(revision.changes).rev;
+    }
+
+    function updateChangedAttrs(newNote) {
+      _.forOwn(newNote, function(value, key) {
+        if ($scope.note[key] !== value) $scope.note[key] = value;
+      });
+    }
+
+    NotesService.changes({
+      continuous: true,
+      filter: function(doc) { return doc._id === id; },
+      onChange: function(changes) {
+        if (isChanged(changes)) NotesService.get(id).then(updateChangedAttrs);
+      }
+    });
 
     $scope.onEditorLoaded = function(editor) {
       editor.setFontSize(16);
       editor.setHighlightActiveLine(false);
     };
 
-    $scope.$watch('note.text', function() {
-      $scope.note.updatedAt = (new Date()).valueOf();
-      NotesService.put($scope.note).then(function(resp) {
-        $scope.note._rev = resp.rev;
-      });
-    });
+    $scope.$watch('note.text', (function() {
+      // Do not trigger watch function until after the first digest cycle
+      var initializing = true;
+      $timeout(function() { initializing = false; });
+
+      return function() {
+        if (initializing) return;
+        $scope.note.updatedAt = (new Date()).valueOf();
+        NotesService.put($scope.note).then(function(resp) {
+          $scope.note._rev = resp.rev;
+        });
+      };
+    })());
   }
 ]);
