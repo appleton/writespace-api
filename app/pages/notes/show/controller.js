18adc8a3a07758e9e92fe51868815a1eb113d047
Andrew Appleton <andrew@floatleft.com>
2014-02-26 11:54:41 +0000

    Disable editior shortcuts, focus editor on selection

--- app/pages/notes/show/controller.js
+++ app/pages/notes/show/controller.js
@@ -9,10 +9,27 @@ angular.module('notes.show', [
   function($scope, note, NotesService) {
     $scope.note = note;
 
+    // Add a shortcut to turn it off in the ace instance
+    var DISABLE_SHORTCUTS = [
+      { win: 'Ctrl-F', mac: 'Command-F' },
+      { win: 'Ctrl-L', mac: 'Command-L' }
+    ];
+
     $scope.onEditorLoaded = function(editor) {
       editor.setFontSize(16);
       editor.setShowPrintMargin(false);
       editor.setHighlightActiveLine(false);
+
+      editor.commands.addCommands(DISABLE_SHORTCUTS.map(function(keys) {
+        return {
+          name: 'disable' + keys.mac,
+          bindKey: keys,
+          exec: function disabled() { return false; },
+          readOnly: true
+        };
+      }));
+
+      editor.focus();
     };
 
     $scope.$watchCollection('note.text', function() {
