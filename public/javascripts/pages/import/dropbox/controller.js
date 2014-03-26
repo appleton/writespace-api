8370254b0a53f3a4e064ddef589357a7b0fe32ee
Andrew Appleton <andrew@floatleft.com>
2014-03-26 17:32:22 +0000

    Dropbox file tree breadcrumb nav

--- public/javascripts/pages/import/dropbox/controller.js
+++ public/javascripts/pages/import/dropbox/controller.js
@@ -10,11 +10,23 @@ angular.module('import.dropbox', [
     $scope.isAuthenticated = dropboxClient.isAuthenticated();
     $scope.files = folderList;
 
-    $scope.parentPath = $stateParams.path.split('/').slice(0, -1).join('/');
-    if ($scope.parentPath[0] !== '/') {
-      $scope.parentPath = '/' + $scope.parentPath;
-    }
-    $scope.isRoot = ($scope.parentPath === $stateParams.path);
+    var currentPath = $stateParams.path || '/';
+    var splitPath = currentPath.split('/');
+    if (currentPath === '/') splitPath = splitPath.slice(1);
+
+    $scope.parents = splitPath.reduce(function(memo, folder) {
+      var prev = memo[memo.length - 1] || { path: '' };
+      var name = '/' + (folder === '' ? 'Dropbox' : folder);
+      var path = (prev.path === '/' ? '' : prev.path) + '/' + folder;
+
+      memo.push({
+        name: name,
+        path: path,
+        isCurrent: (path === currentPath)
+      });
+
+      return memo;
+    }, []);
 
     $scope.authenticate = function() {
       dropboxClient.authenticate();
