cb235692af89b4a8bd0a9657dcd57bbb8148ac03
Andrew Appleton <andrew@floatleft.com>
2014-03-26 09:50:41 +0000

    Add link to go up a level

--- public/javascripts/pages/import/dropbox/controller.js
+++ public/javascripts/pages/import/dropbox/controller.js
@@ -3,11 +3,18 @@
 angular.module('import.dropbox', [
 ]).controller('ImportDropboxController', [
   '$scope',
+  '$stateParams',
   'dropboxClient',
   'folderList',
-  function($scope, dropboxClient, folderList) {
+  function($scope, $stateParams, dropboxClient, folderList) {
     $scope.isAuthenticated = dropboxClient.isAuthenticated();
     $scope.files = folderList;
+
+    $scope.parentPath = $stateParams.path.split('/').slice(0, -1).join('/');
+    if ($scope.parentPath[0] !== '/') {
+      $scope.parentPath = '/' + $scope.parentPath;
+    }
+    $scope.isRoot = ($scope.parentPath === $stateParams.path);
 
     $scope.authenticate = function() {
       dropboxClient.authenticate();
