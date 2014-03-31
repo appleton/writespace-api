81ddc2009e43df9c4a12242d38e569a09b8286bc
Andrew Appleton <andrew@floatleft.com>
2014-03-31 19:01:36 +0100

    Fix dropbox in-app auth

--- public/javascripts/services/dropbox-service.js
+++ public/javascripts/services/dropbox-service.js
@@ -6,8 +6,31 @@ angular.module('dropbox.service', [
   function($window) {
     var DROPBOX_KEY = $window.CONFIG.DROPBOX_KEY;
 
-    return new $window.Dropbox.Client({
+    var client = new $window.Dropbox.Client({
       key: DROPBOX_KEY
     });
+
+    client.authDriver(new $window.Dropbox.AuthDriver.Popup({
+      receiverUrl: 'http://localhost:1337/dropbox-callback.html'
+    }));
+
+    return client;
+  }
+]).run([
+  '$window', '$state', '$stateParams',
+  function($window, $state, $stateParams) {
+
+    $window.addEventListener('message', function receiveMessage(ev) {
+      if (ev.data.match(/dropboxjs_oauth/g)) {
+        // Hack. Should be able to use $state.reload() here but can't due to
+        // https://github.com/angular-ui/ui-router/issues/582
+        $state.transitionTo($state.current, angular.copy($stateParams), {
+          reload: true,
+          inherit: true,
+          notify: true
+        });
+      }
+    });
+
   }
 ]);
