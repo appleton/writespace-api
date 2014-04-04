d521f29be3f7b8e28ed6254f1e3005e6912d681a
Andrew Appleton <andrew@floatleft.com>
2014-04-04 19:05:33 +0100

    Gulpfile indentation

--- gulpfile.js
+++ gulpfile.js
@@ -97,20 +97,20 @@ gulp.task('build:fonts', ['fonts'], function() {
     './public/**/fonts/*.otf'
   ];
   return gulp.src(src)
-             // Remove any nesting from the file path and stick them all in
-             // /fonts
-             .pipe(through.obj(function(data, enc, cb) {
-                var path = data.path;
-                var cwd = process.cwd();
-                var fileName = _.last(path.split(cwd + '/public/components'));
+    // Remove any nesting from the file path and stick them all in
+    // /fonts
+    .pipe(through.obj(function(data, enc, cb) {
+      var path = data.path;
+      var cwd = process.cwd();
+      var fileName = _.last(path.split(cwd + '/public/components'));
 
-                fileName = _.last(fileName.split('/fonts/'));
-                data.path = cwd + '/fonts/' + fileName;
-                this.push(data);
+      fileName = _.last(fileName.split('/fonts/'));
+      data.path = cwd + '/fonts/' + fileName;
+      this.push(data);
 
-                cb();
-              }))
-             .pipe(gulp.dest('./dist/fonts'));
+      cb();
+    }))
+    .pipe(gulp.dest('./dist/fonts'));
 });
 
 gulp.task('build:images', function() {
