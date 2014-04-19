50a25617369a485fc125c9ece578e39e99ab1547
Andrew Appleton <andrew@floatleft.com>
2014-04-19 12:38:29 +0100

    More connections for production

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('newrelic');
 var http = require('http');
 var app = require('./server');
 
+if (process.env.NODE_ENV === 'production') http.globalAgent.maxSockets = 20;
+
 // Start the server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
