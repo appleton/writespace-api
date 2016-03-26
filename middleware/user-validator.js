ad340819c3afcd0ec7e8c0a514eab7f717791746
Andrew Appleton <andrew@floatleft.com>
2016-03-26 11:01:16 +0000

    Format user validation

--- middleware/user-validator.js
+++ middleware/user-validator.js
@@ -1,12 +1,27 @@
 'use strict';
 
+function capitalize(str){
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+
 module.exports = function(req, res, next) {
   req.checkBody('email', 'is required').notEmpty();
   req.checkBody('email', 'is not valid').isEmail();
   req.checkBody('password', 'required').notEmpty();
 
   var errors = req.validationErrors();
-  if (errors) return res.json(422, { errors: errors });
+
+  if (errors) {
+    errors = errors.map((error) => {
+      return {
+        source: { pointer: `data/attributes/${error.param}` },
+        detail: `${capitalize(error.param)} ${error.msg}`
+      }
+    });
+
+    return res.json(422, { errors });
+  }
 
   next();
 };
